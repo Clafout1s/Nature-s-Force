4012c9262f5a99773bf2791fc30c18475da6811c
Clafout1s <clafou.prog@proton.me>
2024-04-04 20:30:02 +0200

    Added mockup gun following mouse movements

--- player.gd
+++ player.gd
@@ -7,10 +7,12 @@ extends CharacterBody2D
 @export var SHOTGUN_VELOCITY = 500
 var shotgun_shot=false
 var actual_shot_velocity=0
+var gun_centre_ecart
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
+func _ready():
+	gun_centre_ecart=Vector2(position.x-$gun.global_position.x,position.x-$gun.global_position.x)
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -24,10 +26,12 @@ func _physics_process(delta):
 	jump_physics()
 
 	move_and_slide()
+	
+	rotate_gun()
 
 func horizontal_physics():
 	var direction = Input.get_axis("left", "right")
-	if direction:
+	if direction and not (shotgun_shot and has_same_sign(velocity.x,actual_shot_velocity)):
 		velocity.x = direction * SPEED 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -44,8 +48,18 @@ func shotgun_physics():
 
 func apply_shotgun_physics():
 	if shotgun_shot:
-		print(actual_shot_velocity," ",velocity.x)
 		actual_shot_velocity-=10/float(100)*float(SHOTGUN_VELOCITY)
 		if actual_shot_velocity<=0:
 			shotgun_shot=false
 			actual_shot_velocity=0
+
+func has_same_sign(f1:float,f2:float):
+	return f1<0 and f2<0 or f1>0 and f2>0
+
+func rotate_gun():
+	var angle=position.angle_to_point(get_global_mouse_position())
+	$gun.position =Vector2(cos(angle), sin(angle))*gun_centre_ecart
+	$gun.look_at(get_global_mouse_position())
+
+		
+		
