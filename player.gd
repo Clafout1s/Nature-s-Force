65f217726d99ef00c6b5115c1d9bce1c7b3d340e
Clafout1s <clafou.prog@proton.me>
2024-04-13 16:21:20 +0200

    Added Burst_Movment class, unrefinded but working

--- player.gd
+++ player.gd
@@ -7,81 +7,81 @@ extends CharacterBody2D
 @export var JUMP_VELOCITY = -500.0
 @export var SHOTGUN_VELOCITY = 1000
 
-
-var movement_list_x=[]
-var movement_list_y=[]
-var shotgun_activator=false
-var shotgun_value=0
-var dash_activator = false
 var gun_centre_ecart
-var down_velocity_modifier=20
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = GRAVITY #ProjectSettings.get_setting("physics/2d/default_gravity")
 var exp_gravity=0
-
-var x_modifier_burst = 0
+var decelereation_constant=20
 var x_modifier_constant=0
-var y_modifier_burst = 0
 var y_modifier_constant=0
 
-var jump_activator
-	
+var active_burst_x=null
+var active_burst_y=null
+
+var burst_list=[]
+var deceleration_list=[]
+var shotgun_burst_x = Burst_Movement.new("shotgun_x","x",500,12,20) #nid,ndimension,nvalue_init,nframes_init,ndeceleration_speed
+var shotgun_burst_y = Burst_Movement.new("shotgun_y","y",-1000,12,20)
 func _ready():
 	gun_centre_ecart=Vector2(position.x-$gun.global_position.x,position.x-$gun.global_position.x)
 	
+
 func _physics_process(delta):
-	
-	jump_input()
+	shotgun()
 	rotate_gun()
-	shotgun_input()
-	#dash_input()
-	
+
 	exp_gravity+=gravity*delta
 	if is_on_floor():
 		exp_gravity=0
 	
-	var to_apply_x=return_apply_regular_burst(movement_list_x)
-	var to_apply_y=return_apply_regular_burst(movement_list_y)
-	
 	x_modifier_constant=return_horizontal_input()
-	x_modifier_burst=burst_test_input()
-	y_modifier_burst=0
 	y_modifier_constant=exp_gravity
-	if jump_activator:
-		y_modifier_constant+=JUMP_VELOCITY
 	
-	for elex in to_apply_x:
-		x_modifier_burst+=elex
-	for eley in to_apply_y:
-		y_modifier_burst+=eley
-	
-	print(x_modifier_burst," ",y_modifier_burst)
-	apply_vertical_velocity(delta,y_modifier_constant,y_modifier_burst)
-	apply_horizontal_velocity(delta,x_modifier_constant,x_modifier_burst)
+	apply_vertical_velocity()
+	apply_horizontal_velocity()
 	move_and_slide()
-	if is_on_floor():
-		#print("in")
-		y_modifier_burst = 0
-		jump_activator=false
 	
-	
-func apply_horizontal_velocity(delta,modifier_constant, modifier_burst):
-	velocity.x = modifier_constant + modifier_burst
-	velocity.x = move_toward(velocity.x, modifier_constant, abs(SPEED))
-	
-func apply_vertical_velocity(delta,modifier_constant, modifier_burst):
-	if modifier_burst==0:
-		velocity.y = modifier_constant
-	else:
-		velocity.y =modifier_burst
-	#y_modifier_burst=move_toward(y_modifier_burst, 0, abs(SPEED*50/float(100)))
-	
-	#print(velocity.y)
-	
-	#print(velocity.y)
-	#y_modifier_burst=0
-
+func apply_horizontal_velocity():
+	if active_burst_x==null:
+		for burst_object in deceleration_list:
+			if burst_object.is_dimension_x():
+				x_modifier_constant+=burst_object.return_decelerate()
+				if burst_object.is_deceleration_over():
+					burst_object.end_decelerate(deceleration_list)
+		velocity.x = x_modifier_constant
+		if x_modifier_constant==0:
+			velocity.x=move_toward(velocity.x,0,decelereation_constant)
 		
+	else:
+		velocity.x =  active_burst_x.return_burst_movement()
+		if active_burst_x.is_decelerating():
+			deceleration_list.append(active_burst_x)
+			active_burst_x=null
+	
+func apply_vertical_velocity():
+	if active_burst_y==null:
+		for burst_object in deceleration_list:
+			if not burst_object.is_dimension_x():
+				y_modifier_constant+=burst_object.return_decelerate()
+				if burst_object.is_deceleration_over():
+					burst_object.end_decelerate(deceleration_list)
+		velocity.y = y_modifier_constant
+		if y_modifier_constant==0:
+			velocity.y=move_toward(velocity.y,0,decelereation_constant)
+	else:
+		velocity.y =  active_burst_y.return_burst_movement()
+		if active_burst_y.is_decelerating():
+			deceleration_list.append(active_burst_y)
+			active_burst_y=null
+
+func shotgun():
+	if Input.is_action_just_pressed("action2") and shotgun_burst_x not in deceleration_list and shotgun_burst_y not in deceleration_list:
+		
+		active_burst_x=shotgun_burst_x
+		active_burst_y=shotgun_burst_y
+		active_burst_x.activate()
+		active_burst_y.activate()
+		print(active_burst_x.to_string())
 func return_horizontal_input():
 	var direction = Input.get_axis("left", "right")
 	if direction:
@@ -91,31 +91,14 @@ func return_horizontal_input():
 		
 func jump_input():
 	if Input.is_action_just_pressed("up") and is_on_floor():
-		jump_activator=true
+		pass
 
 func dash_input():
 	if Input.is_action_just_pressed("action_bar"):
 		var direction = Input.get_axis("left", "right")
 		if direction:
-			regular_burst_launch("dash",direction*100,dash_activator,20,movement_list_x)
+			pass
 	return 0
-
-func regular_burst_launch(id,value,activator:bool,frame_start,movement_list:Array):
-	if not activator and find_in_list(movement_list,id)==null:
-		movement_list.append([id,frame_start,value])
-		activator=true
-		
-func return_apply_regular_burst(movement_list):
-	var final_list=[]
-	for ele in movement_list:
-		var value = ele[2]
-		var frames=ele[1]
-		if frames==0:
-			movement_list.erase(ele)
-		else:
-			ele[1]-=1
-			final_list.append(value)
-	return final_list
 	
 func find_in_list(list,thing):
 	for ele in list:
@@ -126,8 +109,7 @@ func find_in_list(list,thing):
 func shotgun_input():
 	if Input.is_action_just_pressed("action2"):
 		var angle=position.angle_to_point(get_global_mouse_position())
-		regular_burst_launch("shotgun_x",-cos(angle)*SHOTGUN_VELOCITY,shotgun_activator,10,movement_list_x)
-		regular_burst_launch("shotgun_y",-sin(angle)*SHOTGUN_VELOCITY,shotgun_activator,10,movement_list_y)
+		pass
 		
 func burst_test_input():
 	if Input.is_action_just_pressed("action_bar"):
@@ -144,31 +126,69 @@ func rotate_gun():
 	$gun.position =Vector2(cos(angle), sin(angle))*gun_centre_ecart
 	$gun.look_at(get_global_mouse_position())
 
-
-"""
-Objectif: un systeme de velocité x et y indépendatment des commandes: elles ne sont que des arguments spécifiques du syst
-comme le dash
-
-shotgun:  quand c2: on lance shotgun, stocke l'angle
-si shotgun: x_modifier + cos mchin truc,  y_modifier + sin machin truc
-shotgun speed est réduite tout le long
-
-
-func apply_shotgun_x(value):
-	if shotgun:
-		bla bla
-		value+=return
-
-func regular_burst(value,activator:bool,timer:(frames or real timer),burst_list):
-	if not activator:
-		movement_list.append()
-	elif frames==0:
-		movement_list.pop()
-	else:
-		(frames-1)
-		return value
-	return 0
+class Burst_Movement:
+	var id
+	var dimension
+	var active:bool
+	var decelerate:bool
+	var value_init
+	var value
+	var frames_init
+	var frames
+	var deceleration_speed
+	func _init(nid,ndimension,nvalue_init,nframes_init,ndeceleration_speed):
+		self.id=nid
+		self.dimension=ndimension
+		value_init=nvalue_init
+		value=value_init
+		frames_init=nframes_init
+		frames=frames_init
+		deceleration_speed=ndeceleration_speed
+		active=false
+		decelerate=false
+	func _to_string():
+		print(id," ",dimension," ",active," ",decelerate," ",value," ",frames," ",deceleration_speed," ")
+	func is_dimension_x():
+		return dimension=="x"
+	
+	func return_burst_movement():
+		if active:
+			print(frames)
+			frames-=1
+			deactivate_if_finished()
+			return value
+		else:
+			return 0
+	func activate():
+		frames=frames_init
+		active=true
+		value=value_init
+	func deactivate():
+		active=false
+		frames=frames_init
+	func is_activated():
+		return activate
+	func deactivate_if_finished():
+		if frames==0:
+			deactivate()
+			decelerate=true
+		elif frames <0:
+			print("frame countdonwn error")
 		
-"""
-		
+	func start_decelerate(list):
+		list.append(self)
+	func is_decelerating():
+		return decelerate
+	func return_decelerate():
+		if decelerate:
+			value=move_toward(value,0,deceleration_speed)
+			return value
+	func is_deceleration_over():
+		if decelerate:
+			return value==0
+		else:
+			return "not started dingus"
+	func end_decelerate(list):
+		list.remove_at(list.find(self))
+		decelerate=false
 		
