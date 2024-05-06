0ad230a842dc075fac040e246b398d54b5de18d5
Clafout1s <clafou.prog@proton.me>
2024-05-06 17:50:09 +0200

    completely functionnal Regular_value, needs to implement it to player movements

--- Regular_value.gd
+++ Regular_value.gd
@@ -9,9 +9,12 @@ var value_counter = -1
 var tpf
 var activated = false
 var decelerating = false
+var just_finished=false
 var has_deceleration
 var dtps
 var dtpf
+var followup
+var origin
 
 func _init(nid,nvalue_init,ntps,nhas_deceleration=false,ndtps=0):
 	id=nid
@@ -21,37 +24,54 @@ func _init(nid,nvalue_init,ntps,nhas_deceleration=false,ndtps=0):
 	dtps=ndtps
 	
 	tpf = tps * 60
-	if has_deceleration:
-		dtpf = dtps * 60
-
+	dtpf = dtps * 60
+	
+	self.add_origin(self.pack_attributes(self))
+		
 func start():
+	print("start ",id)
 	activated = true
+	decelerating=false
+	just_finished=false
 	value = value_init/tpf
 	value_counter = 0
 
 func end():
+	print("end ",id)
 	if has_deceleration:
 		start_deceleration()
 	else:
 		activated = false
 		reset_values()
+		switch_to_followup()
 
 func return_value():
 	if activated and not decelerating:
 		value_counter+=value
-		if value_counter == value_init:
+		print(value_counter)
+		if abs(value_counter) >= abs(value_init):
 			end()
+			return 0
 		return value
 	elif activated and decelerating:
-		value_counter=move_toward(value_counter,0,value)
+		if just_finished:
+			just_finished = false
+		value_counter=move_toward(value_counter,0,abs(value))
 		if value_counter == 0:
 			end_deceleration()
+			return 0
 		return value_counter
+	else:
+		return 0
 	
 func start_deceleration():
-	decelerating = true
-	value = value_init/ tpf / dtpf
-	value_counter = value
+	if has_deceleration:
+		activated=true
+		decelerating = true
+		value_counter =  value_init/ tpf
+		value = value_init/ tpf / dtpf
+		print(value)
+		
 
 func end_deceleration():
 	decelerating = false
@@ -61,57 +81,58 @@ func end_deceleration():
 func reset_values():
 	value=-1
 	value_counter=-1
-	
-"""
-tps : init
-	value_init : init
-	value=-1
-	value_counter=-1
-	tpf = tps*60
+	tpf = tps * 60
+	dtpf = dtps * 60
+
+func has_just_finished():
+	if just_finished:
+		just_finished=false
+		return true
+	else:
+		return just_finished 
+
+func change_values(nvalue_init,ntps=null,ndtps=null):
+	if not activated or just_finished:
+		if nvalue_init != null:
+			value_init=nvalue_init
+		if ntps != null:
+			tps = ntps
+			tpf = tps * 60
+		if ndtps != null:
+			dtps = ndtps
+			dtpf = dtps * 60
+
+func add_followup(pack):
+	if followup == null:
+		followup=pack
+		origin[5]=pack
+
+func add_origin(pack):
+	if origin == null:
+		origin = pack
+
+func switch_to_followup():
+	if followup != null:
+			unpack_attributes(followup)
+			self.start()
+	elif origin != self.pack_attributes() and origin != null:
+		print("its okay flying gorilla")
+		self.unpack_attributes(origin)
+	else:
+		print("No Maidens ??")
+
+func pack_attributes(other=self):
+	var final_pack=[other.id,other.value_init,other.tps,other.has_deceleration,other.dtps,other.followup]
+	return final_pack
+
+func unpack_attributes(pack):
+	id=pack[0]
+	value_init=pack[1]
+	tps=pack[2]
+	has_deceleration=pack[3]
+	dtps=pack[4]
+	followup=pack[5]
 	activated=false
 	decelerating=false
-	has_deceleration : init
-	dtps: init
-	dtpf=tps*60
-	
-	func start():
-		activated = true
-		value=value_init/tpf
-		value_counter=0
-	func end():
-		
-		if has_deceleration:
-			deceleration_start()
-		else:
-			activated = false
-			reset_values()
-	func return_value():
-		if activated and not decelerating:
-			value_counter+=value
-			if value_counter==value_init:
-				end()
-			return value
-		elif activated and decelerating:
-			value_counter = move_toward(value_counter,0,value)
-			if value_counter==0:
-				end_deceleration()
-			return value_counter
-			
-	func start_deceleration():
-		#activated stays true
-		decelerating = true
-		value=(value_init/tpf)/dtpf
-		value_counter = value
-	
-	func end_deceleration():
-		deceleration = false
-		reset_values()
-		
-	func reset_values():
-		value=-1
-		value_counter=-1
-
-
-
-"""
-
+	just_finished=false
+	reset_values()
--- dummy.gd
+++ dummy.gd
@@ -5,46 +5,41 @@ signal hit
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-var rotation_started
-var rotation_angle=6.28319
+var rotation_angle=0.959931
 var rotation_speed=0.15
-var rotation_value
-var rotation_follow=0
-var rotation_sign=1
+
 var dummy_scale=Vector2(1.5,1.5)
+
+
+var rotation_instance=Regular_value.new("boing",rotation_angle,rotation_speed)
+
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	scale=dummy_scale
-	$CollisionShape2D.set_deferred("disabled",true)
+	rotation_instance.add_followup(rotation_instance.pack_attributes(Regular_value.new("boing_back",-rotation_angle,rotation_speed)))
+	
+	#$CollisionShape2D.set_deferred("disabled",true)
 
 func _physics_process(delta):
 	# Add the gravity.
-	"""
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	"""
+
 	if Input.is_action_just_pressed("action2"):
 		start_rotation()
-	if rotation_started:
-		rotate(rotation_value)
-		rotation_follow+=rotation_value
-	if abs(rotation_follow)>=abs(rotation_angle) and rotation_started:
-		end_rotation()
-	velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	rotate(rotation_instance.return_value())
+	
+	
 	move_and_slide()
 
 func start_rotation():
-	rotation_started= ! rotation_started
-	rotation_follow=0
-
-	rotation_value=rotation_angle/float(rotation_speed * 60)
-	print(rotation_value)
+	rotation_instance.start()
 
 
-func end_rotation():
-	rotation_started=false
-	print(rotation_follow)
-	rotation_follow=0
+func _on_hit():
+	start_rotation()
